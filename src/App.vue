cb80affc26f141c16a1d836fc1ea2c0a055be6bb
hugh <270222838@qq.com>
2020-11-10 23:07:16 +0800

    完善 aside 切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ import {
 export default {
   name: 'App',
   setup() {
-    const asideVisible = ref(false)
+    const width = document.documentElement.clientWidth
+    const asideVisible = ref(width >= 500)
     provide('asideVisible', asideVisible)
   },
 }
