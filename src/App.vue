1b2589a27482c02caf3512b8aa6feb3520ea07e0
hugh <270222838@qq.com>
2020-11-10 18:09:30 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Hugh />
+</template>
+
+<script>
+import Hugh from './components/Hugh.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hugh,
+  },
+}
+</script>
