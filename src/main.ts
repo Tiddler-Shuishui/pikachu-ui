5c6789997cd9b143496b9cbf8a93a8f59c89d52e
hugh <270222838@qq.com>
2020-11-10 19:58:23 +0800

    搭建html结构

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Hugh from "./components/Hugh.vue"
-import Hugh2 from "./components/Hugh2.vue"
+import Home from "./views/Home.vue"
+import Doc from "./views/Doc.vue"
 
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
-    { path: '/', component: Hugh },
-    {path:'/xxx', component: Hugh2}
+    { path: '/', component: Home },
+    {path:'/xxx', component: Doc}
   ]
 })
 
