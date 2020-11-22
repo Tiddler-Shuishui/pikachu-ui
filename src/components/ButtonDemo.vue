64fb19e51b19c376edf9d680f4c67fd3f60bd501
hugh <270222838@qq.com>
2020-11-22 21:47:21 +0800

    给 Button 组件添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,10 @@
 <div>Button 示例</div>
 <h1>示例 1</h1>
 <div>
-  <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small" level="normal">你好</Button>
+  <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
 </div>
 </template>
 
