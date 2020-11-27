e5100b34b79262f27a15de2c396cac404e8d67d0
hugh <270222838@qq.com>
2020-11-27 19:17:37 +0800

    加载中 css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="qing-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="qing-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -21,6 +22,10 @@ export default {
       default: 'normal',
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -163,6 +168,25 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .qing-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: qing-spin 1s infinite linear;
+  }
+}
+@keyframes qing-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
