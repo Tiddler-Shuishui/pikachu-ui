389784d7d650824f59a314765f009da17eade124
hugh <270222838@qq.com>
2020-11-28 15:57:55 +0800

    Dialog 支持 visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="qing-dialog-overlay"></div>
-  <div class="qing-dialog-wrapper">
-    <div class="qing-dialog">
-      <header>标题 <span class="qing-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="qing-dialog-overlay"></div>
+    <div class="qing-dialog-wrapper">
+      <div class="qing-dialog">
+        <header>标题 <span class="qing-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue'
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
   components: {
     Button,
   },
