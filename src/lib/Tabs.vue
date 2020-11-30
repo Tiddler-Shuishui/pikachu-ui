7c06778f4813cd094244c484436cfa91d22e1405
hugh <270222838@qq.com>
2020-11-30 17:14:59 +0800

    使用css切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="qing-tabs">
     <div class="qing-tabs-nav">
-      <div class="qing-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+      <div
+        class="qing-tabs-nav-item"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        :key="index"
+      >
         {{ t }}
       </div>
     </div>
@@ -9,15 +15,22 @@
       <component
         class="qing-tabs-content-item"
         v-for="(c, index) in defaults"
+        :class="{ selected: c.props.title === selected }"
         :key="index"
         :is="c"
       />
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import Tab from './Tab.vue'
+import { computed } from 'vue'
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -25,12 +38,23 @@ export default {
         throw new Error('Tabs 的子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      console.log('重新 return')
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
       titles,
+      current,
+      select,
     }
   },
 }
@@ -57,12 +81,20 @@ $border-color: #d9d9d9;
       }
 
       &.selected {
-        color: $color;
+        color: $blue;
       }
     }
   }
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
