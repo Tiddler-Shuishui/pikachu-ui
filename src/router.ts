a5ad1a1863ad0e51a574143493446c80f69c6400
hugh <270222838@qq.com>
2020-12-13 14:43:59 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,14 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Home from "./views/Home.vue"
-import Doc from "./views/Doc.vue"
-import SwitchDemo from "./components/SwitchDemo.vue"
-import ButtonDemo from "./components/ButtonDemo.vue"
-import DialogDemo from "./components/DialogDemo.vue"
-import DocDemo from "./components/DocDemo.vue"
-import TabsDemo from "./components/TabsDemo.vue"
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
+import DialogDemo from './components/DialogDemo.vue'
+import DocDemo from './components/DocDemo.vue'
+import TabsDemo from './components/TabsDemo.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
+import Intro from './views/Intro.vue'
 const history = createWebHashHistory()
 
 export const router = createRouter({
@@ -13,14 +16,21 @@ export const router = createRouter({
   routes: [
     { path: '/', component: Home },
     {
-      path: '/doc', component: Doc, children: [{ path: '', component: DocDemo },
+      path: '/doc',
+      component: Doc,
+      children: [
+        { path: '', component: DocDemo },
+        { path: 'get-started', component: GetStarted },
+        { path: 'install', component: Install },
+        { path: 'intro', component: Intro },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
-        { path: 'tabs', component: TabsDemo },]
-    }
-  ]
+        { path: 'tabs', component: TabsDemo },
+      ],
+    },
+  ],
 })
 router.afterEach(() => {
-  console.log("路由切换了");
+  console.log('路由切换了')
 })
