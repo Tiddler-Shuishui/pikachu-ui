4d77cee42f7401fbdd5d8af92f1a89761fdcd92a
hugh <270222838@qq.com>
2020-12-16 20:12:41 +0800

    抽离用户需要拷贝的代码

---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <Switch v-model:value="bool" disabled />
+</template>
+<script>
+import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool,
+    }
+  },
+}
+</script>
